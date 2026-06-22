044e96f2d1027cbb7accd5797ebc6d2e14c583b5
linuxmetadata <linuxmeta.data@gmail.com>
2026-06-22 12:47:40 +0530

    final validation fix

--- routes/validate.js
+++ routes/validate.js
@@ -405,13 +405,39 @@ if (directPeriodMatch) {
 
   const simpleRangeMatch =
   text.match(
-    /(\d{1,2}[\/-]\d{1,2}[\/-]\d{2,4})\s*to\s*(\d{1,2}[\/-]\d{1,2}[\/-]\d{2,4})/i
+    /(\d{1,2}[\/-]\d{1,2}[\/-]\d{2,4})\s*(?:to|-)\s*(\d{1,2}[\/-]\d{1,2}[\/-]\d{2,4})/i
   );
 
 if (simpleRangeMatch) {
 
   const startParts =
     simpleRangeMatch[1].split(/[\/-]/);
+
+  const fileMonth =
+    parseInt(startParts[1]);
+
+  let fileYear =
+    parseInt(startParts[2]);
+
+  if (fileYear < 100) {
+    fileYear += 2000;
+  }
+
+  return (
+    fileMonth === month &&
+    fileYear === year
+  );
+}
+
+  const dashRangeMatch =
+  text.match(
+    /(\d{1,2}[\/-]\d{1,2}[\/-]\d{2,4})\s*-\s*(\d{1,2}[\/-]\d{1,2}[\/-]\d{2,4})/
+  );
+
+if (dashRangeMatch) {
+
+  const startParts =
+    dashRangeMatch[1].split(/[\/-]/);
 
   const fileMonth =
     parseInt(startParts[1]);

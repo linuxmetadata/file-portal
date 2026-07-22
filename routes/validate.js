ca75d86abdf8a6e0fb027cae9fdf397776249650
linuxmetadata <linuxmeta.data@gmail.com>
2026-07-22 11:33:40 +0530

    final validation fix

--- routes/validate.js
+++ routes/validate.js
@@ -73,14 +73,14 @@ function validateDateRange(startDate, endDate) {
 async function extractText(filePath, ext) {
 
   try {
-const { PdfParse } = require("pdf-parse");
+const { pdfParse } = require("pdf-parse");
     if (ext === "pdf") {
 
     try {
 
         const buffer = fs.readFileSync(filePath);
 
-        const parser = new PdfParse({ data: buffer });
+        const parser = new pdfParse({ data: buffer });
 
         const data = await parser.getText();
 
@@ -1926,7 +1926,7 @@ router.post(
 
           const buffer = fs.readFileSync(file.path);
 
-          const parser = new PdfParse({ data: buffer });
+          const parser = new pdfParse({ data: buffer });
 
           const data = await parser.getText();
 
